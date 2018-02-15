825d0280c191b6078c9562abb6867599bfc5cc4b
missmakita <mfernanlav@gmail.com>
2018-02-15 15:48:27 -0300

    adds inverse geocodification feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// Geolocation
+// Geolocalización usuario
 const userLocation = document.getElementById('userLocation');
 const locate = document.getElementById('locate');
 
@@ -13,10 +13,21 @@ locate.onclick = function getLocation() {
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
+  let location = '';
   userLocation.innerHTML = `Latitud: ${latitude}
                             <br>Longitud: ${longitude}`;
+  // Geocodificación inversa (devuelve una localidad para la posición):                            
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=locality&key=AIzaSyD3ITDAeNFr0MvgoNVODFh0JJHIxdbch_M`)                          
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data3) {
+      console.log(data3);
+      location = data3.results[0].address_components[0].short_name;
+      console.log(location);
+    })  
 
-  // Retrieve weather forecast for location
+  // Genera el pronóstico para la ubicación del usuario:
   let actual = document.querySelector('.actual');
 
   fetch(`https://api.darksky.net/forecast/ddd476a250882ec17bf7b60f9d91689e/${latitude},${longitude}?lang=es&units=auto`)
@@ -31,17 +42,23 @@ function showPosition(position) {
       // Datos actuales:
       let icon = actual.icon;
       let summary = actual.summary;
-      let temperature = actual.temperature;
-      let apparent = actual.apparentTemperature;
-      let precipProb = actual.precipProbability;
-      let humidity = actual.humidity;
-      let uvIndex = actual.uvIndex;
-      let windSpeed = actual.windSpeed;
+      let temperature = parseInt(actual.temperature);
+      let apparent = parseInt(actual.apparentTemperature); // sensación térmica
+      let precipProb = actual.precipProbability * 100; // probabilidad de precipitaciones
+      let humidity = actual.humidity * 100;
+      let uvIndex = actual.uvIndex; // indice UV
+      let windSpeed = actual.windSpeed; // velocidad del viento
 
       $('.actual').append(`
-      <canvas id="iconActual" width="128" height="128"></canvas>
-      <span>${temperature}°</span>
+        <span>${location}</span>
+        <canvas id="iconActual" width="128" height="128"></canvas>
+        <span>${temperature}°</span>
         <span>${summary}</span>
+        <span>Temperatura ambiente ${apparent}°</span>
+        <span>Probabilidad de lluvia ${precipProb}%</span>
+        <span>Humedad ambiente ${humidity}%</span>
+        <span>Índice UV ${uvIndex}</span>
+        <span>Velocidad del viento${windSpeed}</span>
       `);
 
       // Agrega ícono actual:
